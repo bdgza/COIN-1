ea4102cd6b9545095f135d5251e78fc92c0b5e49
Bart de Groot <bdegroot@gmail.com>
2017-08-16 10:01:33 +0100

    Added serialization methods to classes.

--- src/common/playerInteraction.js
+++ src/common/playerInteraction.js
@@ -5,6 +5,22 @@ class PlayerInteraction {
         this.respondingFactionId = definition.respondingFactionId;
         this.status = definition.status || 'requested';
     }
+
+    toJSON() {
+        return {
+            type: this.type,
+            requestingFactionId: this.requestingFactionId,
+            respondingFactionId: this.respondingFactionId,
+            status: this.status
+        };
+    }
+
+    loadGameState(json) {
+        this.type = json.type;
+        this.requestingFactionId = json.requestingFactionId;
+        this.respondingFactionId = json.respondingFactionId;
+        this.status = json.status;
+    }
 }
 
 export default PlayerInteraction;
--- src/fallingsky/interactions/retreatDeclaration.js
+++ src/fallingsky/interactions/retreatDeclaration.js
@@ -7,6 +7,18 @@ class RetreatDeclaration extends PlayerInteraction {
 
         this.regionId = definition.regionId;
     }
+
+    toJSON() {
+        return Object.assign(super.toJSON(), {
+            regionId: this.regionId
+        });
+    }
+
+    loadGameState(json) {
+        super.loadGameState(json);
+
+        this.regionId = json.regionId;
+    }
 }
 
 export default RetreatDeclaration;
